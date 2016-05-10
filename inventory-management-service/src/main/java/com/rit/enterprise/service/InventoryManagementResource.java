3e6d8ccfeaa5a77512f6a278eda250168615ad5d
Taylor <tnc5484@rit.edu>
2016-05-09 21:28:47 -0400

    so we can add the front end

--- inventory-management-service/src/main/java/com/rit/enterprise/service/InventoryManagementResource.java
+++ inventory-management-service/src/main/java/com/rit/enterprise/service/InventoryManagementResource.java
@@ -20,8 +20,8 @@ public class InventoryManagementResource {
     private final ProductDao productDao;
     private final LoggingDao loggingDao;
 
-    HttpClient httpClient = HttpClients.createDefault();
-    HttpPost   httpPost = new HttpPost("http://vm343a.se.rit.edu/accounting/inventory");
+    // HttpClient httpClient = HttpClients.createDefault();
+    // HttpPost   httpPost = new HttpPost("http://vm343a.se.rit.edu/accounting/inventory");
 
     @Inject
     public InventoryManagementResource(ProductDao productDao, LoggingDao loggingDao) {
@@ -85,17 +85,17 @@ public class InventoryManagementResource {
         return productDao.getProducts();
     }
 
-    @POST
-    @Path("/purchase/{id}/{amount}")
-    public HttpResponse purchaseParts(@PathParam("id") int id,
-                                    @PathParam("amount") int increaseAmount
-                                    @QueryParam("transactionId") Integer transactionId){
-        productDao.increaseStockQuantityForProductId(id, increaseAmount);
-        loggingDao.insertLogging(transactionId, "Purchase", LocalDateTime.now(), id, increaseAmount);
-        double costOfGoods = productDao.getBaseCostForProductId(id) * amount;
-        InventoryRequest request = new InventoryRequest(costOfGoods, transactionId);
-        httpPost.setEntity(new URLEncodedFormEntity(request, "UTF-8"));
-        HttpResponse response = httpClient.execute(httpPost);
-        return response;
+    // @POST
+    // @Path("/purchase/{id}/{amount}")
+    // public HttpResponse purchaseParts(@PathParam("id") int id,
+    //                                 @PathParam("amount") int increaseAmount
+    //                                 @QueryParam("transactionId") Integer transactionId){
+    //     productDao.increaseStockQuantityForProductId(id, increaseAmount);
+    //     loggingDao.insertLogging(transactionId, "Purchase", LocalDateTime.now(), id, increaseAmount);
+    //     double costOfGoods = productDao.getBaseCostForProductId(id) * amount;
+    //     InventoryRequest request = new InventoryRequest(costOfGoods, transactionId);
+    //     httpPost.setEntity(new URLEncodedFormEntity(request, "UTF-8"));
+    //     HttpResponse response = httpClient.execute(httpPost);
+    //     return response;
     }
 }
